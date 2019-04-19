393a858b2ff8d3dd498e4ba1b4f3d81397a3573f
zjy-github <3375388698@qq.com>
2019-04-19 17:06:10 +0800

    4.19.3

--- src/routes/seat/components/SeatSelected.jsx
+++ src/routes/seat/components/SeatSelected.jsx
@@ -6,20 +6,28 @@ const SeatSelected = ({ data, onRemove }) => {
 
   return (  
     <div className="seatSelected">
-      <ul className="seatSelected__list">
-      {
-        data.map(seat => (
-        <li key={seat.id} className="seatSelected__item movieTicket">
-          <div className="movieTicket__detail">
-            <div className="movieTicket__pos">{seat.rowIndex}排{seat.colIndex}座</div>
-            <div className="movieTicket__price"><i className="movieTicket__tag">卡</i> 33元</div>
-          </div>
-          <div className="movieTicket__close" onClick={() => onRemove(seat.id)}></div>
-        </li> 
-        ))
-      }
-      </ul>
-      <div className="seatSelected__buy">{33 * data.length}元 确认选座</div>
+    {
+      data.length?
+          (
+            <div>
+              <ul className="seatSelected__list">
+                {
+                  data.map(seat => (
+                  <li key={seat.id} className="seatSelected__item movieTicket">
+                    <div className="movieTicket__detail">
+                      <div className="movieTicket__pos">{seat.rowIndex}排{seat.colIndex}座</div>
+                      <div className="movieTicket__price"><i className="movieTicket__tag">卡</i> 33元</div>
+                    </div>
+                    <div className="movieTicket__close" onClick={() => onRemove(seat.id)}></div>
+                  </li> 
+                  ))
+                }
+              </ul>
+              <div className="seatSelected__buy">{33 * data.length}元 确认选座</div>
+            </div>
+          )
+          : ''
+    }
     </div>
   );
 };
--- src/routes/seat/index.jsx
+++ src/routes/seat/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import MovieInof from './components/MovieInfo';
 import SeatSelected from './components/SeatSelected';
 import SeatSelector from './container/SeatSelector';
